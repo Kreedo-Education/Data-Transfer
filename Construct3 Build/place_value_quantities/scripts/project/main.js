a5ce737f40dd31c37a1c472b85a7167850585c2d
KreedoEducation <79310045+KreedoEducation@users.noreply.github.com>
2021-06-22 11:25:20 +0530

    zip changed

--- Construct3 Build/place_value_quantities/scripts/project/main.js
+++ Construct3 Build/place_value_quantities/scripts/project/main.js
@@ -30,7 +30,7 @@ document.addEventListener("message", handleEvent);
 function handleEvent(messageTemp) {	
 	isWeb = false;
 	messageData =  messageTemp.data;	
-	alert("Data from webview", messageData);
+	alert("Data from webview:", JSON.stringify(messageData));
 }
 
 runOnStartup(async runtime =>
